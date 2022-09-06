bba0fb92a82e7052d2a079bc26f27f2d5e224ba1
check-the-kitchen <ilia.kibin@gmail.com>
2022-09-06 22:28:04 +0300

    task6

---
+++ Task6.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace src;
+
+class Task6
+{
+    public function main(int $year, int $lastYear, int $month, int $lastMonth, $day = 'Monday'): int
+    {
+        if ($year < 0 || $lastYear < 0 || $month > 12 || $month < 1 || $lastMonth > 12 || $lastMonth < 1) {
+            throw new \InvalidArgumentException('Function only accepts integers.');
+        }
+        $date1 = new \DateTimeImmutable($year.'-'.$month.'-01');
+        $date2 = new \DateTimeImmutable($lastYear.'-'.$lastMonth.'-01');
+        $counter = 0;
+        while ($date1->getTimestamp() != $date2->getTimestamp()) {
+            if ($date1->format('D') == 'Mon') {
+                $counter++;
+            }
+            $date1 = $date1->add(new \DateInterval('P1M'));
+        }
+
+        return $counter;
+    }
+}
