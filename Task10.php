27b88b7513a31e87d130d6d95d581d7215e73ab1
Ilia <ilia.kibin@gmail.com>
2022-09-07 12:14:43 -0700

    cs fixed

--- Task10.php
+++ Task10.php
@@ -6,8 +6,15 @@ class Task10
 {
     public function main(int $number): array
     {
+        if ($number <= 0) {
+            throw new \InvalidArgumentException('Please, pass number that is bigger than 1');
+        }
+
         $result = $number;
         $resultArray = [$number];
+        if ($result == 1) {
+            return $resultArray;
+        }
         while ($result != 1) {
             if ($result % 2 == 1) {
                 $result = $result * 3 + 1;
